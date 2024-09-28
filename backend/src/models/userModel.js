455fe3dd18abe05532962a457348065caada73df
colloso999 <tripperskripper@gmail.com>
2024-09-28 13:33:27 +0300

    created a model for the user

---
+++ backend/src/models/userModel.js
@@ -0,0 +1,13 @@
+import db from '.config/database.js'
+import bcrypt from 'bcrypt'
+
+export default class User {
+  static async create({email, password, first_name, last_name}) {
+    const hashedPassword = await bcrypt.hash(password, 10)
+    return db.one(`
+      INSERT INTO users (email, password_hash, first_name, last_name)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id, email, first_name, last_name
+    `, [email, hashedPassword, first_name, last_name])
+  }
+}
