d467900b9ed264bf72bd1efdb01e6370bc79626b
unknown <mohamedhikal943@gmail.com>
2024-10-16 10:02:08 +0300

    add history

--- modules/phone_place_kenya_scraping.py
+++ modules/phone_place_kenya_scraping.py
@@ -10,6 +10,192 @@ from datetime import datetime
 # from clean_jumia_data import clean_jumia_data  # Ensure this module is available
 
 
+# class PhonePlaceKenyaScraping:
+#     def __init__(self):
+#         # Initialize the WebDriver (ensure the ChromeDriver is in PATH)
+#         self.driver = webdriver.Chrome()
+#         self.all_product_data = []
+
+#     def extract_info(self, text):
+#         """
+#         Helper function to extract information from product details.
+#         """
+#         lines = text.strip().split('\n')
+#         product_info = {}
+#         for line in lines:
+#             if ':' in line:
+#                 key, value = line.split(':', 1)
+#                 product_info[key.strip()] = value.strip()
+#             else:
+#                 if 'Reviews' in line:
+#                     product_info['Reviews'] = line.strip()
+#                 elif 'IN STOCK' in line:
+#                     product_info['Status'] = line.strip()
+#                 elif 'Warranty' in line:
+#                     product_info['Warranty Info'] = line.strip()
+#                 elif line.startswith("East Africa") or line.startswith("UAE"):
+#                     product_info['Region'] = line.strip()
+#                 elif line.startswith("KSh"):
+#                     if 'PhonePlaceKenya_oldPrice' not in product_info:
+#                         # First price
+#                         product_info['PhonePlaceKenya_oldPrice'] = line.strip()
+#                     else:
+#                         # Second price
+#                         product_info['PhonePlaceKenya_Price'] = line.strip()
+#         return product_info
+
+#     def scrape_phones_data(self, phone_type):
+#         """
+#         Scrapes phone data for a given phone type from PhonePlace Kenya website.
+#         """
+#         # Base URL
+#         url = f"https://www.phoneplacekenya.com/product-category/smartphones/{phone_type}/"
+
+#         self.driver.get(url)
+
+#         try:
+#             # Wait for the search results to load
+#             phones_grid = WebDriverWait(self.driver, 20).until(
+#                 EC.presence_of_all_elements_located(
+#                     (By.CLASS_NAME, "product-wrapper"))
+#             )
+
+#             # Loop through each product element
+#             for i, product in enumerate(phones_grid):
+#                 try:
+#                     # Re-locate product wrapper in case of stale reference
+#                     phones_grid = WebDriverWait(self.driver, 20).until(
+#                         EC.presence_of_all_elements_located(
+#                             (By.CLASS_NAME, "product-wrapper"))
+#                     )
+
+#                     product = phones_grid[i]  # Access product element by index
+
+#                     # Extract the product link before clicking
+#                     product_link_element = product.find_element(
+#                         By.TAG_NAME, 'a')
+#                     product_link = product_link_element.get_attribute('href')
+
+#                     product.click()  # Click on the product
+
+#                     # Wait for the product details to load (single element)
+#                     phone_info = WebDriverWait(self.driver, 20).until(
+#                         EC.presence_of_element_located(
+#                             (By.CLASS_NAME, "product-images-summary"))
+#                     )
+
+#                     # Product information
+#                     product_name = phone_info.find_element(
+#                         By.CSS_SELECTOR, ".product_title.entry-title").text
+
+#                     data = phone_info.find_element(
+#                         By.CSS_SELECTOR, ".summary.entry-summary").text
+
+#                     # Extract the information and append to the list
+#                     product_data = self.extract_info(data)
+#                     # Add product name to the data
+#                     product_data['productName'] = product_name
+
+#                     # Add the product link to the data
+#                     # New Field
+#                     product_data['PhonePlaceKenya_productLink'] = product_link
+
+#                     # Add the current date and time to the product data
+#                     product_data['Scrape Date'] = datetime.now().strftime(
+#                         "%Y-%m-%d")  # Date
+#                     product_data['Scrape Time'] = datetime.now().strftime(
+#                         "%H:%M:%S")  # Time
+
+#                     # Extract discount percentage (if available)
+#                     try:
+#                         discount_percent = phone_info.find_element(
+#                             By.CLASS_NAME, "onsale.percent").text
+
+#                         product_data['PhonePlaceKenya_Discount'] = discount_percent
+#                     except:
+#                         product_data['PhonePlaceKenya_Discount'] = "No Discount"
+
+#                     # Extract verifiedRatings (if available)
+#                     try:
+#                         verifiedRatings = phone_info.find_element(
+#                             By.CLASS_NAME, "review-count").text
+
+#                         product_data['PhonePlaceKenya_verifiedRatings'] = verifiedRatings
+#                     except:
+#                         product_data['PhonePlaceKenya_verifiedRatings'] = 0
+
+#                     self.all_product_data.append(product_data)
+
+#                     time.sleep(2)  # Pause before navigating back
+#                     self.driver.back()  # Go back to the previous page
+#                     time.sleep(2)
+
+#                 except Exception as e:
+#                     print(f"Error occurred on product {i+1}: {e}")
+
+#         except TimeoutException as e:
+#             print(f"Failed to load product grid: {e}")
+
+#     def reorder_columns(self):
+#         """
+#         Reorders the DataFrame columns as per the required format.
+#         """
+#         df = pd.DataFrame(self.all_product_data)
+
+#         # Required columns in specific order
+#         ordered_columns = ['Scrape Date',
+#                            'Scrape Time', 'productName', 'PhonePlaceKenya_productLink']  # Added link here
+
+#         # Add other columns except 'Price 1' and 'Price 2'
+#         other_columns = [col for col in df.columns if col not in [
+#             'Scrape Date', 'Scrape Time', 'productName', 'PhonePlaceKenya_oldPrice', 'PhonePlaceKenya_Price', 'PhonePlaceKenya_Discount', 'PhonePlaceKenya_verifiedRatings', 'PhonePlaceKenya_productLink']]
+
+#         # Add 'Price 1' and 'Price 2' at the end
+#         ordered_columns += other_columns + \
+#             ['PhonePlaceKenya_verifiedRatings', 'PhonePlaceKenya_oldPrice', 'PhonePlaceKenya_Price',
+#              'PhonePlaceKenya_Discount', 'PhonePlaceKenya_productLink']  # Ensure link is included
+
+#         # Reorder the DataFrame
+#         df_reordered = df[ordered_columns]
+#         return df_reordered
+
+#     def save_data_to_csv(self, df, file_path):
+#         """
+#         Saves the DataFrame to a CSV file.
+#         """
+#         df.to_csv(file_path, index=False)
+#         print(f"Data successfully saved to {file_path}")
+
+#     def close_driver(self):
+#         """
+#         Closes the WebDriver instance.
+#         """
+#         self.driver.quit()
+
+
+# # Example usage
+# if __name__ == "__main__":
+#     # Create an instance of the class
+#     scraper = PhonePlaceKenyaScraping()
+
+#     # List of phone types to scrape
+#     phone_types = ["samsung", "iphone", "infinix-phones-in-kenya", "google", "itel", "nokia",
+#                    "oppo", "oneplus", "realme", "tecno", "vivo", "xiaomi"]
+
+#     # Loop through the phone types and scrape data
+#     for phone in phone_types:
+#         scraper.scrape_phones_data(phone)
+
+#     # Reorder the columns and get the final DataFrame
+#     final_df = scraper.reorder_columns()
+#     print(final_df.sample(3))
+
+#     # Save the DataFrame to CSV
+#     scraper.save_data_to_csv(
+#         final_df, r'D:\freelance\FairPriceKe-add_llm\FairPriceKe\FairPriceKe\data\Phone Place Kenya Scraping\row\scraped_phones_with_datetime.csv')
+
+#     # Close the browser after scraping
+#     scraper.close_driver()
 class PhonePlaceKenyaScraping:
     def __init__(self):
         # Initialization and other methods remain the same...
--- modules/scraping_and_put_in_sql.py
+++ modules/scraping_and_put_in_sql.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlalchemy
 from sqlalchemy import text  # Import text for executing raw SQL
-from get_jumia_phones_data import *  
+from get_jumia_phones_data import *
 from get_phones_kenya_data import PhonePlaceKenyaScraping
 from clean_phone_kenya_data import PhoneKenyaDataCleaner
 from clean_jumia_data import JumiaDataCleaner
@@ -11,10 +11,10 @@ from sqlalchemy import create_engine, inspect
 class ConvertDfToSQL:
     def __init__(self):
         self.user = 'root'
-        self.password = 'mmm_321478910'
+        self.password = '1234'
         self.host = 'localhost'
         self.db_name = 'FAIRPRICEKE'.lower()
-   
+
         # Create engine without specifying the database to create it if necessary
         self.engine = sqlalchemy.create_engine(
             f'mysql+pymysql://{self.user}:{self.password}@{self.host}')
@@ -24,10 +24,8 @@ class ConvertDfToSQL:
         # Create the database if it doesn't exist
         try:
             with self.engine.connect() as conn:
-                # Use text() for raw SQL
                 conn.execute(
                     text(f"CREATE DATABASE IF NOT EXISTS {self.db_name}"))
-                # Use text() for raw SQL
                 conn.execute(text(f"USE {self.db_name}"))
 
             # Update the engine to connect to the new database
@@ -35,45 +33,53 @@ class ConvertDfToSQL:
                 f'mysql+pymysql://{self.user}:{self.password}@{self.host}/{self.db_name}')
         except sqlalchemy.exc.SQLAlchemyError as e:
             print(f"Error creating database: {e}")
-            
-    def convert(self, df: pd.DataFrame, table_name: str):
+
+    def convert(self, df: pd.DataFrame, table_name: str, mode='replace'):
         # Create table and insert data
         try:
-            df.to_sql(table_name, con=self.engine,
-                      if_exists='replace', index=False)
+            df.to_sql(table_name, con=self.engine, if_exists=mode, index=False)
             print(f"Data inserted into table {table_name}")
         except sqlalchemy.exc.SQLAlchemyError as e:
             print(f"Error inserting data into table {table_name}: {e}")
-            
+
+    def append_to_history(self, df: pd.DataFrame, history_table_name: str):
+        # Append data to the history table
+        try:
+            df.to_sql(history_table_name, con=self.engine,
+                      if_exists='append', index=False)
+            print(f"Data appended to history table {history_table_name}")
+        except sqlalchemy.exc.SQLAlchemyError as e:
+            print(
+                f"Error appending data to history table {history_table_name}: {e}")
+
     def run(self):
+        # Scraping for PhonePlaceKenya
         ppk = PhonePlaceKenyaScraping()
         df = ppk.run()
         ppk_clean = PhoneKenyaDataCleaner(df)
         df_cleaned = ppk_clean.clean_data()
-        # Convert DataFrame to SQL table and create the database if it doesn't exist
-        converter = ConvertDfToSQL(
-            user=self.user, password=self.password, host=self.host, db_name=self.db_name)
-        converter.convert(df_cleaned, 'PhonePlaceKenya'.lower())
+
+        # Replace old data in PhonePlaceKenya table and append to history
+        self.convert(df_cleaned, 'PhonePlaceKenya'.lower(), mode='replace')
+        self.append_to_history(df_cleaned, 'PhonePlaceKenya_history'.lower())
 
         #############################################################
-        
-        
+
+        # Scraping and cleaning for Jumia
         cleaner = JumiaDataCleaner(
             r'data\Jumia\2024-10-03_all_brands_products.json')
-        # Get the cleaned DataFrame
         cleaned_df = cleaner.get_cleaned_data()
         cleaned_df.Key_Features = cleaned_df.Key_Features.apply(
             lambda x: json.dumps(x))
         cleaned_df.jumia_stock = cleaned_df.jumia_stock.apply(
             lambda x: json.dumps(x))
 
-        converter = ConvertDfToSQL()
-        converter.convert(cleaned_df, 'jumia'.lower())
+        # Replace old data in jumia table and append to history
+        self.convert(cleaned_df, 'jumia'.lower(), mode='replace')
+        self.append_to_history(cleaned_df, 'jumia_history'.lower())
 
 
 # Example usage
 if __name__ == "__main__":
-    
     x = ConvertDfToSQL()
     x.run()
-    # print(x.analyze_jumia_table())
